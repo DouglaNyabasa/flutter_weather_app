5b05346581e051146b15f859cdd1b8d2428c6ebf
douglanyabasa <douglasnyabasa400@gmail.com>
2025-11-19 13:02:46 +0200

    finished implementing the search screen

--- lib/widgets/famous_city_tile.dart
+++ lib/widgets/famous_city_tile.dart
@@ -3,50 +3,66 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_report_app/Provider/get_weather_by_city_provider.dart';
 import 'package:weather_report_app/contants/app_colors.dart';
+import 'package:weather_report_app/utils/get_weather_icons.dart';
+
+import '../contants/text_styles.dart';
 
 class FamousCityTile extends ConsumerWidget {
-  const FamousCityTile(   {super.key, required this.city, required this.index,});
+  const FamousCityTile({super.key, required this.city, required this.index});
   final String city;
   final int index;
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final weatherData = ref.watch(getWeatherByCityNameProvider(city));
-    return  weatherData.when(
-      data: (weather){
+    return weatherData.when(
+      data: (weather) {
         return Material(
-          color: index ==0 ? AppColors.lightBlue : AppColors.accentBlue,
+          color: index == 0 ? AppColors.lightBlue : AppColors.accentBlue,
           borderRadius: BorderRadius.circular(25.0),
-          child: Padding(padding: EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 24,
-          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 24),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   children: [
-                    Column(
-                      children: [
-                        Text(weather.main.temp.round().toString()),
-                        const SizedBox(height: 10,),
-                        Text(weather.weather[0].description),
-                      ],
-                    )
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${weather.main.temp.round().toString()}°',
+                            style: TextStyles.h2,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+
+                            weather.weather[0].description,
+                            style: TextStyles.subtitleText,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Image.asset(
+                        width: 50,
+                        getWeatherIcon(weatherCode: weather.weather[0].id))
                   ],
-                )
+                ),
+                Text(weather.name,style: TextStyle(fontSize: 18,color: Colors.white.withOpacity(.8),fontWeight: FontWeight.w400),)
               ],
             ),
           ),
         );
       },
-      error: (error,stackTrace){
+      error: (error, stackTrace) {
         return Center(child: Text(error.toString()));
       },
-      loading: (){
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
+      loading: () {
+        return const Center(child: CircularProgressIndicator());
       },
-
     );
   }
 }
